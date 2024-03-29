442207c89d4f2e9daf036f772cfb0e3ea60b1492
Axel García <axldorian3@gmail.com>
2024-03-29 13:40:26 -0600

    Update [set current path]

--- Avance1/A.r
+++ Avance1/A.r
@@ -1,7 +1,8 @@
 library(here)
 
-# Establecer directorio de trabajo (raíz del proyecto)
-set_here(path='..')
+# # Establecer directorio de trabajo 
+# # (solo si el directorio actual no es la raíz del proyecto)
+# set_here(path='..')
 
 # Ruta al dataset
 dataset_path <- here("Datasets", "Avance1_A.csv")
